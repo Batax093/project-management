8fa5d3fa3575901b06a63883b2c52200e0ba30ee
Fredrik Sahalatua Pakpahan <fredrikspakpahan@gmail.com>
2024-12-12 16:59:04 +0700

    ready for deploy and fix bug port

--- server/src/app.ts
+++ server/src/app.ts
@@ -37,9 +37,9 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
 // Server 
-const PORT = process.env.PORT || 8000;
+const PORT = Number(process.env.PORT) || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
